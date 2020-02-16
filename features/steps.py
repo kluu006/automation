65184891c0e315235692bbf5f431fd8370120c58
Kenny <kluu006@ucr.edu>
2020-02-16 06:11:31 -0800

    Added driver class, defined steps for first scenario, defined steps for browser launch and close

--- features/steps.py
+++ features/steps.py
@@ -1,16 +1,31 @@
 from lettuce import *
-from selenium import webdriver
 from time import sleep
+from selenium import webdriver
+import driver
 
-# create a new google chrome session
-driver = webdriver.Chrome()
 
-# navigate to reddit
-driver.get("https://old.reddit.com/")
+@before.all
+def set_browser():
+    # create a new google chrome session
+    driver.driver = webdriver.Chrome()
+    # navigate to reddit
+    driver.driver.get("https://old.reddit.com/")
 
-# finds the element for /r/all and clicks the link
-r_all_link = driver.find_element_by_xpath("//a[@href='https://old.reddit.com/r/all/']")
-r_all_link.click()
 
-sleep(3)
-driver.close()
+@step('I click on r_all')
+def click_r_all(step):
+    # finds the element for /r/all and clicks the link
+    r_all_link = driver.driver.find_element_by_xpath("//a[@href='https://old.reddit.com/r/all/']")
+    r_all_link.click()
+
+
+@step('I navigate to r_all')
+def navigate_r_all(step):
+    # validate if on /r/all
+    assert driver.driver.current_url == "https://old.reddit.com/r/all/", driver.driver.current_url
+
+
+@after.all
+def close_browser(step):
+    sleep(3)
+    driver.driver.close()
--- features/test.feature
+++ features/test.feature
@@ -2,6 +2,5 @@ Feature: Navigate through reddit
     Browse favorite subreddits
 
     Scenario: Navigate to r_all
-        Given I am on reddit
         When I click on r_all
         Then I navigate to r_all
