c4618666514472fb134141c13a0cd1c2ca281672
Kenny <kluu006@ucr.edu>
2020-02-21 04:01:50 -0800

    maximized window, added try/except in instances of no web element, added Scenario: 'Clicking on the header image on the banner sends you to the home page'

--- features/steps.py
+++ features/steps.py
@@ -3,6 +3,7 @@ from time import sleep
 from selenium import webdriver
 import driver
 import subreddit
+from selenium.common.exceptions import NoSuchElementException
 
 
 @before.all
@@ -11,6 +12,7 @@ def set_browser():
     driver.driver = webdriver.Chrome()
     # navigate to reddit
     driver.driver.get("https://old.reddit.com/")
+    driver.driver.maximize_window()
 
 
 @step('I click on r_all')
@@ -20,28 +22,48 @@ def click_r_all(step):
     r_all_link.click()
 
 
-@step('I navigate to r_all')
+@step('I am on r_all')
 def navigate_r_all(step):
     # validate if on /r/all
     assert driver.driver.current_url == "https://old.reddit.com/r/all/", driver.driver.current_url
 
 
-@step('I have the following (\S+)')
+@step('I have the following (\S+) subreddit')
 def have_the_subreddits(step, sub):
     subreddit.name = sub
 
 
-@step('I click on (\S+)')
+@step('I click on (\S+) subreddit')
 def click_subreddit(step, sub):
     # finds the element for the subreddit and clicks the link
-    link = driver.driver.find_element_by_xpath("//a[@href='https://old.reddit.com/r/%s/']" % sub)
-    link.click()
+    try:
+        link = driver.driver.find_element_by_xpath("//a[@href='https://old.reddit.com/r/%s/']" % sub)
+        link.click()
+    except NoSuchElementException:
+        raise Exception("Element does not exist to be clicked.")
 
 
-@step('I navigate to (\S+)')
+@step('I navigate to (\S+) subreddit')
 def navigate_to_subreddit(step, sub):
     # validate if on subreddit
     assert driver.driver.current_url == "https://old.reddit.com/r/%s/" % subreddit.name, driver.driver.current_url
+
+
+@step('Then I click on the header image')
+def click_on_header_image(step):
+    # click on the header image in the top banner
+    try:
+        image = driver.driver.find_element_by_id("header-img-a")
+
+        image.click()
+    except NoSuchElementException:
+        raise Exception("Element does not exist to be clicked.")
+
+
+@step('I am on the home page')
+def navigate_to_home_page(step):
+    # validate if on the home page
+    assert driver.driver.current_url == "https://old.reddit.com/", driver.driver.current_url
 
 
 @after.all
--- features/test.feature
+++ features/test.feature
@@ -3,16 +3,28 @@ Feature: Navigate through reddit
 
     Scenario: Navigate to r_all
         When I click on r_all
-        Then I navigate to r_all
+        Then I am on r_all
 
      @sub
      Scenario: Navigate to subreddit
-        Given I have the following <subreddit>
-        When I click on <subreddit>
-        Then I navigate to <subreddit>
+        Given I have the following <subreddit> subreddit
+        When I click on <subreddit> subreddit
+        Then I navigate to <subreddit> subreddit
 
         Examples:
             | subreddit |
             | AskReddit |
             | funny     |
 
+    @home
+     Scenario: Clicking on the header image on the banner sends you to the home page
+        Given I have the following <subreddit> subreddit
+        When I click on <subreddit> subreddit
+        Then I navigate to <subreddit> subreddit
+        Then I click on the header image
+        Then I am on the home page
+
+        Examples:
+            | subreddit |
+            | news    |
+            | aww       |
